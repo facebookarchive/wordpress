1af7ec20d9da0278e327a830e7b1d0b8b04c7b2f
Matt Kelly <matthwk@gmail.com>
2012-04-10 18:09:04 -0700

    Added OG tags, but still needs some work

--- wp/wp-content/plugins/facebook/fb_core.php
+++ wp/wp-content/plugins/facebook/fb_core.php
@@ -7,7 +7,7 @@
 Plugin Name: Facebook
 Plugin URI: [TODO]
 Description: [TODO]
-Author: Facebook, Matt Kelly
+Author: Facebook; Matt Kelly (matthwk), James Pearce (jamesgpearce)
 Version: 0.1a
 Author URI: http://developers.facebook.com/
 License: [TODO]
@@ -18,7 +18,28 @@ require_once('fb_social_plugins.php');
 
 //wp_enqueue_script('fb_js_sdk', plugins_url('/js/fb.js', __FILE__));
 
-add_action ( 'wp_loaded', 'fb_js_init' );
+add_action ( 'get_footer', 'fb_js_init' );
+
+add_action ( 'wp_head', 'fb_add_og_protocol' );
+
+function fb_add_og_protocol() {
+	
+	print '<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+     <meta property="og:type"                 content="article"> 
+     <meta property="og:site_name"            content="' . esc_attr(get_bloginfo()) . '">
+     <meta property="og:image"                content="URL to an image">
+     <meta property="og:title"                content="Name of article">
+     <meta property="og:description"          content="Description of object"> 
+     <meta property="article:published_time"  content="DateTime"> 
+     <meta property="article:modified_time"   content="DateTime"> 
+     <meta property="article:expiration_time" content="DateTime">
+     <meta property="article:author"          content="URL to Author object">
+     <meta property="article:section"         content="Section of article">
+     <meta property="article:tag"             content="Keyword">';
+		 
+}
+
+//<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
 
 function fb_js_init() {
 	print '<div id="fb-root"></div>
@@ -26,7 +47,7 @@ function fb_js_init() {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=413161128711224";
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, "script", "facebook-jssdk"));</script>';
 }

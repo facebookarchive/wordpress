a195d97182e80484d1957c36d2e8a82a9923077b
Niall Kennedy <niall@niallkennedy.com>
2012-05-13 18:33:48 +0000

    always output locale, site_name, type, and fb:app_id. special article treatment for posts

--- wp/wp-content/plugins/facebook/fb_open_graph.php
+++ wp/wp-content/plugins/facebook/fb_open_graph.php
@@ -27,67 +27,78 @@ function fb_output_og_protocol( $property, $content ) {
 	}
 }
 
+/**
+ * Add Open Graph protocol markup to <head>
+ *
+ * @since 1.0
+ */
 function fb_add_og_protocol() {
 	global $post;
 
-	// change me when you are ready for more than just posts
-	if ( ! is_single() )
-		return;
+	$meta_tags = array(
+		'http://ogp.me/ns#locale' => fb_get_locale(),
+		'http://ogp.me/ns#site_name' => get_bloginfo( 'name' ),
+		'http://ogp.me/ns#type' => 'website'
+	);
 
-	$meta_tags = array();
+	if ( is_home() || is_front_page() ) {
+		$meta_tags['http://ogp.me/ns#description'] = get_bloginfo( 'description' );
+	} else if ( is_single() ) {
+		$post_type = get_post_type();
+		$meta_tags['http://ogp.me/ns#type'] = 'article';
+		$meta_tags['http://ogp.me/ns#url'] = apply_filters( 'rel_canonical', get_permalink() );
+		if ( post_type_supports( $post_type, 'title' ) )
+			$meta_tags['http://ogp.me/ns#title'] = get_the_title();
+		if ( post_type_supports( $post_type, 'excerpt' ) )
+			$meta_tags['http://ogp.me/ns#description'] = apply_filters( 'the_excerpt', get_the_excerpt() );
+		$meta_tags['http://ogp.me/ns/article#published_time'] = get_the_date('c');
+		$meta_tags['http://ogp.me/ns/article#modified_time'] = get_the_modified_date('c');
+		if ( is_multi_author() && post_type_supports( $post_type, 'author' ) )
+			$meta_tags['http://ogp.me/ns/article#author'] = get_author_posts_url( get_the_author_meta('ID') );
 
-	$meta_tags['http://ogp.me/ns#locale'] = fb_get_locale();
-	$meta_tags['http://ogp.me/ns#site_name'] = get_bloginfo( 'name' );
-	$meta_tags['http://ogp.me/ns#type'] = 'article';
-	$meta_tags['http://ogp.me/ns#url'] = get_permalink();
-	$meta_tags['http://ogp.me/ns#title'] = get_the_title();
-	$meta_tags['http://ogp.me/ns#description'] = apply_filters( 'the_excerpt', get_the_excerpt() );
-	$meta_tags['http://ogp.me/ns/article#published_time'] = get_the_date('c'); 
-	$meta_tags['http://ogp.me/ns/article#modified_time'] = get_the_modified_date('c'); 
-	$meta_tags['http://ogp.me/ns/article#author'] = get_author_posts_url( get_the_author_meta('ID') );
-
-	$cat_ids = get_the_category();
-	if ( ! empty( $cat_ids ) ) {
-		$cat = get_category( $cat_ids[0] );
-		if ( ! empty( $cat ) )
-			$meta_tags['http://ogp.me/ns/article#section'] = $cat->name;
-
-		/*
-		TODO: output the rest of the categories as tags
-		unset( $cat_ids[0] );
+		$cat_ids = get_the_category();
 		if ( ! empty( $cat_ids ) ) {
-			foreach( $cat_ids as $cat_id ) {
-				$cat = get_category( $cat_id );
-				$article->addTag( $cat->name );
-				unset( $cat );
+			$cat = get_category( $cat_ids[0] );
+			if ( ! empty( $cat ) )
+				$meta_tags['http://ogp.me/ns/article#section'] = $cat->name;
+
+			/*
+			TODO: output the rest of the categories as tags
+			unset( $cat_ids[0] );
+			if ( ! empty( $cat_ids ) ) {
+				foreach( $cat_ids as $cat_id ) {
+					$cat = get_category( $cat_id );
+					$article->addTag( $cat->name );
+					unset( $cat );
+				}
+			} */
+		}
+
+		// TODO: add tags. treat tags as lower priority than multiple categories
+		// $meta_tags['http://ogp.me/ns/article#tag'] = '';
+
+		// does current post type and the current theme support post thumbnails?
+		if ( post_type_supports( $post_type, 'thumbnail' ) && function_exists( 'has_post_thumbnail' ) && has_post_thumbnail() ) {
+			list( $post_thumbnail_url, $post_thumbnail_width, $post_thumbnail_height ) = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+			if ( ! empty( $post_thumbnail_url ) ) {
+				$image = array( 'url' => $post_thumbnail_url );
+
+				if ( ! empty( $post_thumbnail_width ) )
+					$image['width'] = absint( $post_thumbnail_width );
+
+				if ( ! empty($post_thumbnail_height) )
+					$image['height'] = absint( $post_thumbnail_height );
+				$meta_tags['http://ogp.me/ns#image'] = array( $image );
 			}
-		} */
-	}
-
-	// TODO: add tags. treat tags as lower priority than multiple categories
-	// $meta_tags['http://ogp.me/ns/article#tag'] = '';
-
-	// does theme support post thumbnails?
-	if ( function_exists( 'has_post_thumbnail' ) && has_post_thumbnail() ) {
-		list( $post_thumbnail_url, $post_thumbnail_width, $post_thumbnail_height ) = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
-		if ( ! empty( $post_thumbnail_url ) ) {
-			$image = array( 'url' => $post_thumbnail_url );
-
-			if ( ! empty( $post_thumbnail_width ) )
-				$image['width'] = absint( $post_thumbnail_width );
-
-			if ( ! empty($post_thumbnail_height) )
-				$image['height'] = absint( $post_thumbnail_height );
-			$meta_tags['http://ogp.me/ns#image'] = array( $image );
 		}
 	}
 
 	$options = get_option( 'fb_options' );
 	if ( ! empty( $options['app_id'] ) )
 		$meta_tags['http://ogp.me/ns/fb#app_id'] = $options['app_id'];
-	
+
 	$meta_tags = apply_filters( 'fb_meta_tags', $meta_tags, $post );
-	
+
 	foreach ( $meta_tags as $property => $content ) {
 		fb_output_og_protocol( $property, $content );
 	}

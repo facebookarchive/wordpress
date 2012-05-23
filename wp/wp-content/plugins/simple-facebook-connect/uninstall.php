4677e2c0c3eec1c1ac31aab08505b074fd557364
Matt Kelly <matthwk@gmail.com>
2012-05-23 15:25:21 -0700

    Removing WP install, so the repo is only the Facebook plugin.

--- wp/wp-content/plugins/simple-facebook-connect/uninstall.php
+++
@@ -1,5 +0,0 @@
-<?php
-if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
-    exit();
-
-delete_option('sfc_options');
--- wp/wp-content/themes/graphene/admin/editor.css.php
+++
@@ -1,5 +0,0 @@
-<?php if ( isset( $_GET['content_width'] ) ) : $content_width = $_GET['content_width']; ?>
-.wp-editor {
-    width: <?php echo $content_width; ?>px;
-}
-<?php endif; ?>

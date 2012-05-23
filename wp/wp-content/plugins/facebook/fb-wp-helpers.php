91efb5a60a2a10985b06b247f767af63e7268f73
James Pearce <jpearce@fb.com>
2012-05-23 15:03:37 -0700

    Label override supported in widget forms...
    …plus:
    
     - layouts more consistent with standard WordPress widget forms
     - some refactoring

--- wp/wp-content/plugins/facebook/fb-wp-helpers.php
+++ wp/wp-content/plugins/facebook/fb-wp-helpers.php
@@ -33,79 +33,74 @@ function fb_construct_fields($placement, $children, $parent = null, $object = nu
 
 function fb_construct_fields_children($placement, $children, $parent = null, $object = null) {
 	$return = '';
+	$checkbox = '<input class="checkbox"';
 
-	if ($placement == 'widget') {
+	if ( $placement == 'widget' ) {
 		$children_output = '';
 
 		$instance = $object->get_settings();
 
-		foreach ($children as $child) {
+		foreach ( $children as $child ) {
 			$object_id = $object->number;
 
-			if (isset($instance[$object_id][$child['name']])) {
-				$child_value = $instance[$object_id][$child['name']];
+			$name = $child['name'];
+		  $name_label = isset($child['label']) ? $child['label'] : ucfirst(str_replace("_", " ", $name));
+			$name_field_id = $object->get_field_id( $name );
+			$name_field_name = $object->get_field_name( $name );
+			$help = $child['help_text'];
+
+			$value = '';
+			if ( isset($instance[$object_id][$name]) ) {
+				$value = $instance[$object_id][$name];
 			}
-			elseif (isset($child['default']) && !$parent['enable']) {
-				$child_value = $child['default'];
+			elseif ( isset($child['default']) && !$parent['enable'] ) {
+				$value = $child['default'];
 			}
-			else {
-				$child_value = '';
-			}
+			$value = esc_attr($value);
 
 			$help_link = '';
-
 			if (!isset($child['help_link'])) {
-				$help_link = '<a href="#" target="_new" title="' . $child['help_text'] . '" onclick="return false;" style="color: #aaa; text-decoration: none;">[?]</a> &nbsp;';
+				$help_link = '<a href="#" target="_new" title="' . $help . '" onclick="return false;" style="color: #aaa; text-decoration: none;">[?]</a>';
 			}
 			else {
-				$help_link = '<a href="' . $child['help_link'] . '" target="_new" title="' . $child['help_text'] . '" style=" text-decoration: none;">[?]</a> &nbsp;';
+				$help_link = '<a href="' . $child['help_link'] . '" target="_new" title="' . $help . '" style=" text-decoration: none;">[?]</a>';
 			}
+
+			$label = '<label for="' . $name_field_id . '">' . $name_label . '</label>';
 
 			$children_output .= '<p>';
 			switch ($child['field_type']) {
+
 				case 'dropdown':
-					$children_output .= $help_link . ' <label for="' . $object->get_field_id( $child['name'] ) . '">' . ucwords(str_replace("_", " ", $child['name'])) . '</label> &nbsp;';
-
-					$children_output .= '<select name="' . $object->get_field_name( $child['name'] ) . '" id="' . $object->get_field_id( $child['name'] ) . '">';
-
-					if (isset($child_value)) {
-						foreach ($child['options'] as $key => $val) {
-							$children_output .= '<option value="' . $key . '" ' . selected( $child_value, $key, false ) . '>' . $val . '</option>';
-						}
+					$children_output .= "$label: $help_link<br />";
+					$children_output .= '<select name="' . $name_field_name . '" id="' . $name_field_id . '" class="widefat">';
+					foreach ($child['options'] as $key => $val) {
+						$children_output .= '<option value="' . $key . '" ' . selected( $value, $key, false ) . '>' . ucfirst(str_replace("_", " ", $val)) . '</option>';
 					}
-					else {
-						foreach ($child['options'] as $key => $val) {
-							$children_output .= '<option value="' . $key . '">' . $val . '</option>';
-						}
-					}
-
-					$children_output .= '</select>';
-
+					$children_output .= "</select>";
 					break;
+
 				case 'checkbox':
-					$children_output .= $help_link . '<input class="checkbox" id="' . $object->get_field_id( $child['name'] ) . '" name="' . $object->get_field_name( $child['name'] ) . '" type="checkbox" value="true"' . checked($child_value, 'true', false) . '>';
-					$children_output .= ' <label for="' . $object->get_field_id( $child['name'] ) . '">' . ucwords(str_replace('_', ' ', $child['name'])) . '</label><br>';
-
+					$children_output .= $checkbox . ' id="' . $name_field_id . '" name="' . $name_field_name . '" type="checkbox" value="true"' . checked($value, 'true', false) . '>';
+					$children_output .= " $label $help_link<br/>";
 					break;
+
 				case 'text':
 					$text_field_value = '';
-
-					$children_output .= $help_link . ' <label for="' . $object->get_field_id( $child['name'] ) . '">' . ucwords(str_replace('_', ' ', $child['name'])) . '</label> &nbsp;';
-
-					$children_output .= '<input id="' . $object->get_field_id( $child['name'] ) . '" name="' . $object->get_field_name( $child['name'] ) . '" type="text" value="' . $child_value . '">';
-
+					$children_output .= "$label: $help_link<br />";
+					$children_output .= '<input id="' . $name_field_id . '" name="' . $name_field_name . '" type="text" value="' . $value . '" class="widefat">';
 					break;
+
 				case 'disabled_text':
 					$text_field_value = '';
-
-					$children_output .= $help_link . ' <label for="' . $object->get_field_id( $child['name'] ) . '">' . ucwords(str_replace('_', ' ', $child['name'])) . '</label> &nbsp;';
-
+					$children_output .= "$help_link $label &nbsp;";
 					$children_output .= $child['disabled_text'];
-
 					break;
 			}
-
 			$children_output .= '</p>';
+
+			$children_output = str_replace('<br/></p><p>$checkbox', '<br/>$checkbox', $children_output);
+			// Tighly group checkboxes.
 
 			$return['output'] = $children_output;
 		}
@@ -146,24 +141,24 @@ function fb_construct_fields_children($placement, $children, $parent = null, $ob
 				$parent_js_array = '[' . $parent['name'] . ']';
 
 				if (isset($options[$parent['name']][$child['name']])) {
-					$child_value = $options[$parent['name']][$child['name']];
+					$value = $options[$parent['name']][$child['name']];
 				}
 				elseif (isset($child['default']) && empty($options[$parent['name']]['enabled'])) {
-					$child_value = $child['default'];
+					$value = $child['default'];
 				}
 				else {
-					$child_value = '';
+					$value = '';
 				}
 			}
 			else {
 				if (isset($options[$child['name']])) {
-					$child_value = $options[$child['name']];
+					$value = $options[$child['name']];
 				}
 				elseif (isset($child['default'])) {
-					$child_value = $child['default'];
+					$value = $child['default'];
 				}
 				else {
-					$child_value = '';
+					$value = '';
 				}
 			}
 
@@ -175,9 +170,9 @@ function fb_construct_fields_children($placement, $children, $parent = null, $ob
 
 					$children_output .= '<select name="fb_options' . $parent_js_array . '[' . $child['name'] . ']">';
 
-					if (isset($child_value)) {
+					if (isset($value)) {
 						foreach ($child['options'] as $key => $val) {
-							$children_output .= '<option value="' . $key . '" ' . selected( $child_value, $key, false ) . '>' . $val . '</option>';
+							$children_output .= '<option value="' . $key . '" ' . selected( $value, $key, false ) . '>' . $val . '</option>';
 						}
 					}
 					else {
@@ -192,14 +187,14 @@ function fb_construct_fields_children($placement, $children, $parent = null, $ob
 				case 'checkbox':
 					$children_output .= '	<tr valign="top"' . $display . ' id="' . $parent['name'] . '_' . $child['name'] . '">
 							<th scope="row">' . ucwords(str_replace('_', ' ', $child['name'])) . '</th>
-							<td>' . $help_link . '&nbsp; <input type="checkbox" name="fb_options' . $parent_js_array . '[' . $child['name'] . ']" value="true"' . checked($child_value, 'true', false) . '></td>
+							<td>' . $help_link . '&nbsp; <input type="checkbox" name="fb_options' . $parent_js_array . '[' . $child['name'] . ']" value="true"' . checked($value, 'true', false) . '></td>
 							</tr>';
 					break;
 				case 'text':
 					$text_field_value = '';
 
-					if (isset($child_value)) {
-						$text_field_value = $child_value;
+					if (isset($value)) {
+						$text_field_value = $value;
 					}
 
 					$children_output .= '	<tr valign="top"' . $display . ' id="' . $parent['name'] . '_' . $child['name'] . '">

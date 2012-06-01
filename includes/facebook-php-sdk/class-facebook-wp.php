5fe8848cdad4029d5b0b620e238852d4511f69f9
Niall Kennedy <niall@niallkennedy.com>
2012-06-01 02:28:21 +0000

    extend Facebook class. replace makeRequest with a WP_HTTP version

---
+++ includes/facebook-php-sdk/class-facebook-wp.php
@@ -0,0 +1,55 @@
+<?php
+
+if ( ! class_exists( 'Facebook' ) )
+	require_once( dirname( __FILE__ ) . '/facebook.php' );
+
+/**
+ * Override default Facebook PHP SDK behaviors with WordPress-friendly features
+ *
+ * @since 1.0
+ */
+class Facebook_WP extends Facebook {
+	/**
+	 * Version
+	 *
+	 * @since 1.0
+	 * @var string
+	 */
+	const VERSION = '3.1.1-wp';
+
+	/**
+	 * Override Facebook PHP SDK cURL function with WP_HTTP
+	 * Facebook PHP SDK is POST-only
+	 *
+	 * @since 1.0
+	 * @todo add file upload support if we care
+	 * @param string $url request URL
+	 * @param array $params parameters used in the POST body
+	 * @param CurlHandler $ch Initialized curl handle. unused: here for compatibility with parent method parameters only
+	 * @return string HTTP response body
+	 */
+	protected function makeRequest( $url, $params, $ch=null ) {
+		global $wp_version;
+
+		var_dump( $wp_version );
+		if ( empty( $url ) || empty( $params ) )
+			return '';
+
+		$params = array(
+			'redirection' => 0,
+			'httpversion' => '1.1',
+			'timeout' => 60,
+			'user-agent' => apply_filters( 'http_headers_useragent', 'WordPress/' . $wp_version . '; ' . get_bloginfo( 'url' ) . '; facebook-php-' . self::VERSION . '-wp' ),
+			'headers' => array( 'Expect:' ),
+			'sslverify' => false, // warning: might be overridden by 'https_ssl_verify' filter
+			'body' => http_build_query( $params, '', '&' )
+		);
+
+		$response = wp_remote_post( $url, $params );
+		if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != '200' )
+			return '';
+
+		return wp_remote_retrieve_body( $response );
+	}
+}
+?>

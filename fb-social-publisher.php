4a4024bc309b6428036f0130c1fc29b271b7b1df
Matt Kelly <matthwk@gmail.com>
2012-06-07 17:59:02 -0700

    Fixed PHP errors.

--- fb-social-publisher.php
+++ fb-social-publisher.php
@@ -169,7 +169,7 @@ function fb_add_fan_page_message_box_save( $post_id ) {
 
 	// OK, we're authenticated: we need to find and save the data
 
-	add_post_meta($post_id, 'fb_fan_page_message', sanitize_text_field$_POST['fb_fan_page_message_box_message']), true);
+	add_post_meta($post_id, 'fb_fan_page_message', sanitize_text_field($_POST['fb_fan_page_message_box_message']), true);
 }
 
 /**
@@ -222,7 +222,7 @@ function fb_post_to_fb_page($post_id) {
 	try {
 		$publish_result = $facebook->api('/' . $fan_page_info[0][2] . '/feed', 'POST', $args);
 
-		add_post_meta($post_id, 'fb_fan_page_post_id', sanitize_text_field$publish_result['id']), true);
+		add_post_meta($post_id, 'fb_fan_page_post_id', sanitize_text_field($publish_result['id']), true);
 	}
 	catch (FacebookApiException $e) {
 	}

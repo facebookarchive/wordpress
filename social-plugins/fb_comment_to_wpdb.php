19e842cb8ede0bab2fab6d96cfa0f6ba182e9242
root <root@saturn.example.com>
2012-08-10 16:26:01 -0400

    new branch for fb style comments

---
+++ social-plugins/fb_comment_to_wpdb.php
@@ -0,0 +1,14 @@
+<?php
+
+//require_once('../facebook.php');
+//Facepile -- 
+//Remove weird avtars at btotom left
+//Fix WP and FB concurrent login issue, mimic intense debate
+//global $facebook;
+// Get User ID
+//$user = $facebook->getUser();
+
+//echo "PHP now has: " . $_POST['fb_comment_id'];
+//echo $user;
+
+?>

59f7b06bb016243a721bf7a675bd1c52997a7648
Colm Doyle <colmdoyle@fb.com>
2012-08-16 00:04:38 +0100

    convert spaces to tabs
    I've fixed any lines that blame to me, but looks like the codebase is
    riddled with tabVspace issues.

--- fb-open-graph.php
+++ fb-open-graph.php
@@ -39,12 +39,12 @@ function fb_strip_and_format_desc( $post ) {
 		$desc_no_html = $post->post_content; // Start over, this time with the post_content
 		$desc_no_html = strip_shortcodes( $desc_no_html ); // Strip shortcodes first in case there is HTML inside the shortcode
 		$desc_no_html = str_replace(']]>', ']]&gt;', $desc_no_html); // Angelo Recommendation, if for some reason ]]> happens to be in the_content, rare but We've seen it happen
-    $desc_no_html = wp_strip_all_tags($desc_no_html);
-    $desc_length = strlen($desc_no_html);
-    $desc_no_html = substr($desc_no_html, 0, 255);
-    if ( $desc_no_html > $desc_length ) {
-      $desc_no_html .= '...';
-    }
+	$desc_no_html = wp_strip_all_tags($desc_no_html);
+	$desc_length = strlen($desc_no_html);
+	$desc_no_html = substr($desc_no_html, 0, 255);
+	if ( $desc_no_html > $desc_length ) {
+		$desc_no_html .= '...';
+	}
 		$desc_no_html = trim($desc_no_html); // Trim the final string, we may have stripped everything out of the post so this will make the value empty if that's the case
 	}
 

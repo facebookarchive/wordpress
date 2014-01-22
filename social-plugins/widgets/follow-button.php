4bbe5cd67a5a3123c1e906827ef084417ddd72e4
Niall Kennedy <niall@niallkennedy.com>
2014-01-22 18:04:00 +0000

    do not escape widget title. matches behavior of default widgets

--- social-plugins/widgets/follow-button.php
+++ social-plugins/widgets/follow-button.php
@@ -54,7 +54,7 @@ class Facebook_Follow_Button_Widget extends WP_Widget {
 		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
 
 		if ( $title )
-			echo $before_title . esc_html( $title ) . $after_title;
+			echo $before_title . $title . $after_title;
 
 		echo $follow_button_html;
 
